91f11c9bd90419da8e36001caa28414c50a2569a
Theerawat Sirisawat <sealactuary128@gmail.com>
2026-03-04 15:07:33 +0700

    Add server.js to set up Express server and load environment variables

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config(path: './config/config.env');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
