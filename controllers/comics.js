a36ab4634dffe357795e2e55cc37341185352309
Theerawat Sirisawat <sealactuary128@gmail.com>
2026-03-12 02:31:01 +0700

    Refactor comics controller to implement async operations and improve error handling

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,49 +1,112 @@
+const Comic = require('../models/Comic');
+
 //@desc     Get all comics
 //@route    GET /api/v1/comics
 //@access   Public
-exports.getComics = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Show all comics'
-    });
+exports.getComics = async (req, res, next) => {
+    try {
+        const comics = await Comic.find();
+        res.status(200).json({
+            success: true,
+            count: comics.length,
+            data: comics
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 //@desc     Get one comic
 //@route    GET /api/v1/comics/:id
 //@access   Public
-exports.getComic = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show comic with id ${req.params.id}`
-    });
+exports.getComic = async (req, res, next) => {
+    try {
+        const comic = await Comic.findById(req.params.id);
+
+        if (!comic) {
+            return res.status(404).json({
+                success: false,
+                error: 'Comic not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: comic
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 //@desc     Create new comic
 //@route    POST /api/v1/comics
 //@access   Public
-exports.createComic = (req, res, next) => {
+exports.createComic = async (req, res, next) => {
+    const comic = await Comic.create(req.body);
     res.status(201).json({
         success: true,
-        msg: 'Create new comic'
+        data: comic
     });
 }
 
 //@desc     Update comic
 //@route    PUT /api/v1/comics/:id
 //@access   Public
-exports.updateComic = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Update comic with id ${req.params.id}`
-    });
+exports.updateComic = async (req, res, next) => {
+    try {
+        const comic = await Comic.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!comic) {
+            return res.status(404).json({
+                success: false,
+                error: 'Comic not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: comic
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 //@desc     Delete comic
 //@route    DELETE /api/v1/comics/:id
 //@access   Public
-exports.deleteComic = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Delete comic with id ${req.params.id}`
-    });
+exports.deleteComic = async (req, res, next) => {
+    try {
+        const comic = await Comic.findByIdAndDelete(req.params.id);
+
+        if (!comic) {
+            return res.status(404).json({
+                success: false,
+                error: 'Comic not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
