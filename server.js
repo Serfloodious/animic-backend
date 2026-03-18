1922e4ef8771f751428ddfa4b613e6423cbb7d1c
Theerawat Sirisawat <sealactuary128@gmail.com>
2026-03-19 03:58:39 +0700

    Add logout functionality and enhance security measures in server setup

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 // Load environment variables from .env file
 dotenv.config({path: './config/config.env'});
@@ -18,6 +24,33 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Sanitize data
+app.use((req, res, next) => {
+    mongoSanitize.sanitize(req.body);
+    mongoSanitize.sanitize(req.params);
+    mongoSanitize.sanitize(req.query);
+    next();
+});
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Cookie parser
 app.use(cookieParser());
