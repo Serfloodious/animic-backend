1769a84a8e35b38f6912a28b7a3a3c9599b71e92
Theerawat Sirisawat <sealactuary128@gmail.com>
2026-03-21 21:03:41 +0700

    Add update details, update password, and delete account routes to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, getMe, logout} = require('../controllers/auth');
+const {register, login, getMe, updateDetails, updatePassword, logout, deleteAccount} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,6 +8,9 @@ const {protect} = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.get('/logout', protect, logout);
+router.delete('/deleteaccount', protect, deleteAccount);
 
 module.exports = router;
